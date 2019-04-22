1dd70a0ceb74be7ad9a27156db4a6c525bcc948b
Saar <saar2666@gmail.com>
2019-04-22 16:39:50 -0700

    added Selenium Test

---
+++ Test.py
@@ -0,0 +1,4 @@
+import selenium
+from selenium import webdriver
+driver = webdriver.Chrome (executable_path='C:\\Users\\Saar\\Documents\\GitHub\\Project\\chromedriver.exe')
+driver.get("http://192.168.99.100:5000/")
